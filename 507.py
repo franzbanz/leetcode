9f24b1bbb2ad3b8f4e56b70b4b318f8ff03fb5ff
franz <franz@koehler-kn.de>
2024-09-19 14:40:04 +0200

    Update 507.py

--- 507.py
+++ 507.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        tmp = 1
+        if num <= 1:
+            return False
+        for i in range(2, int(sqrt(num)) + 1):
+            if num % i == 0:
+                tmp += i
+                if i != num // i:
+                    tmp += num // i
+        if tmp == num:
+            return True
+        return False
